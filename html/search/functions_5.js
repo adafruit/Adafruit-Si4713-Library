a61ad798f9fbbeefc574469c60d19969c349841c
Doxygen CI <ci-arduino@invalid>
2022-10-25 00:39:57 +0000

    Deploy docs to GitHub Pages from commit a398e23a13
    Commit: a398e23a135c9a5a0647842703d25fbba0094b08
    GitHub Actions run: 3317273319

--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -3,7 +3,7 @@ var searchData=
   ['setgpio',['setGPIO',['../class_adafruit___si4713.html#a0a665e113cca98f6a36565bfa39887c2',1,'Adafruit_Si4713']]],
   ['setgpioctrl',['setGPIOctrl',['../class_adafruit___si4713.html#adf827f7b60ea9696efba4b58afe1e5bb',1,'Adafruit_Si4713']]],
   ['setproperty',['setProperty',['../class_adafruit___si4713.html#ac89e0dcc24954f8b474a5375c3321167',1,'Adafruit_Si4713']]],
-  ['setrdsbuffer',['setRDSbuffer',['../class_adafruit___si4713.html#a7fddf5c4ccc16e8d36b29b1620e92d0a',1,'Adafruit_Si4713']]],
-  ['setrdsstation',['setRDSstation',['../class_adafruit___si4713.html#aefe239a879b600efefde1346fcef84c9',1,'Adafruit_Si4713']]],
+  ['setrdsbuffer',['setRDSbuffer',['../class_adafruit___si4713.html#a49c1940f8e684ef585e46468df77a580',1,'Adafruit_Si4713']]],
+  ['setrdsstation',['setRDSstation',['../class_adafruit___si4713.html#ac2fc3e894c85c08aa765175c3f71bceb',1,'Adafruit_Si4713']]],
   ['settxpower',['setTXpower',['../class_adafruit___si4713.html#a8745eee5a58f03cfae4862deef8e7017',1,'Adafruit_Si4713']]]
 ];
